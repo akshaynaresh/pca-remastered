c6206a2a15bb2f8e3e2d6378111b9b43e14afd5e
akshaynaresh <akshaydwivedi0606@gmail.com>
2024-02-13 06:24:35 +0530

    Javascript used

---
+++ script.js
@@ -0,0 +1,15 @@
+let count = 0;
+const countEl = document.getElementById("count-el");
+const saveEl = document.getElementById("save-el");
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  count = 0;
+  countEl.textContent = count;
+}
